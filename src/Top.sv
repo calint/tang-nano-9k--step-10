4bab7b2dee6c674ee7b1f2f3d8f21dbf25c5d2ee
calint <calin.tenitchi@gmail.com>
2024-06-15 10:41:08 +0200

    .

--- src/Top.sv
+++ src/Top.sv
@@ -99,7 +99,6 @@ module Top (
   wire [31:0] ramio_data_out;
   wire ramio_data_out_ready;
   wire ramio_busy;
-  reg [5:0] ramio_leds;
 
   RAMIO #(
       .RAM_DEPTH_BITWIDTH(RAM_DEPTH_BITWIDTH),
@@ -109,7 +108,9 @@ module Top (
       .BAUD_RATE(9_600)
   ) ramio (
       .rst_n(sys_rst_n && rpll_lock && br_init_calib),
-      .clk(br_clk_out),
+      .clk  (br_clk_out),
+
+      // interface
       .enable(ramio_enable),
       .write_type(ramio_write_type),
       .read_type(ramio_read_type),
@@ -118,7 +119,10 @@ module Top (
       .data_out(ramio_data_out),
       .data_out_ready(ramio_data_out_ready),
       .busy(ramio_busy),
-      .leds(ramio_leds),
+
+      .leds(led[5:2]),
+
+      // UART
       .uart_tx(uart_tx),
       .uart_rx(uart_rx),
 
@@ -133,177 +137,28 @@ module Top (
   );
 
   // ----------------------------------------------------------
-  localparam STARTUP_WAIT = 1_000_000;
+  // -- Core
+  // ----------------------------------------------------------
 
-  // localparam FLASH_TRANSFER_BYTES_NUM = 32'h0020_0000;
-  localparam FLASH_TRANSFER_BYTES_NUM = 32'h0010_0000;
+  Core core (
+      .rst_n(sys_rst_n && rpll_lock && br_init_calib),
+      .clk  (br_clk_out),
+      .led  (led[1:0]),
 
-  // used while reading flash
-  reg [23:0] flash_data_to_send;
-  reg [4:0] flash_bits_to_send;
-  reg [31:0] flash_counter;
-  reg [7:0] flash_current_byte_out;
-  reg [7:0] flash_current_byte_num;
-  reg [7:0] flash_data_in[4];
+      .ramio_enable(ramio_enable),
+      .ramio_write_type(ramio_write_type),
+      .ramio_read_type(ramio_read_type),
+      .ramio_address(ramio_address),
+      .ramio_data_in(ramio_data_in),
+      .ramio_data_out(ramio_data_out),
+      .ramio_data_out_ready(ramio_data_out_ready),
+      .ramio_busy(ramio_busy),
 
-  // used while reading flash to increment 'cache_address'
-  reg [31:0] ramio_address_next;
-
-  localparam STATE_INIT_POWER = 0;
-  localparam STATE_LOAD_CMD_TO_SEND = 1;
-  localparam STATE_SEND = 2;
-  localparam STATE_LOAD_ADDRESS_TO_SEND = 3;
-  localparam STATE_READ_DATA = 4;
-  localparam STATE_START_WRITE = 5;
-  localparam STATE_WRITE = 6;
-  localparam STATE_TEST_1 = 7;
-  localparam STATE_TEST_2 = 8;
-  localparam STATE_DONE = 9;
-
-  reg [4:0] state = 0;
-  reg [4:0] return_state = 0;
-
-  always_ff @(posedge br_clk_out, negedge sys_rst_n) begin
-    if (!sys_rst_n || !rpll_lock || !br_init_calib) begin
-
-      flash_counter <= 0;
-      flash_clk <= 0;
-      flash_mosi <= 0;
-      flash_cs <= 1;
-
-      ramio_enable <= 0;
-      ramio_read_type <= 0;
-      ramio_write_type <= 0;
-      ramio_address <= 0;
-      ramio_address_next <= 0;
-      ramio_data_in <= 0;
-
-      led[4:0] <= 5'b1_1111;
-
-      state <= STATE_INIT_POWER;
-
-    end else begin
-      led[5] <= ~ramio_busy;
-
-      case (state)
-
-        STATE_INIT_POWER: begin
-          if (flash_counter > STARTUP_WAIT) begin
-            flash_counter <= 0;
-            state <= STATE_LOAD_CMD_TO_SEND;
-          end else begin
-            flash_counter <= flash_counter + 1;
-          end
-        end
-
-        STATE_LOAD_CMD_TO_SEND: begin
-          flash_cs <= 0;
-          flash_data_to_send[23-:8] <= 3;  // command 3: read
-          flash_bits_to_send <= 8;
-          state <= STATE_SEND;
-          return_state <= STATE_LOAD_ADDRESS_TO_SEND;
-        end
-
-        STATE_LOAD_ADDRESS_TO_SEND: begin
-          flash_data_to_send <= 0;  // address 0x0
-          flash_bits_to_send <= 24;
-          flash_current_byte_num <= 0;
-          state <= STATE_SEND;
-          return_state <= STATE_READ_DATA;
-        end
-
-        STATE_SEND: begin
-          if (flash_counter == 0) begin
-            // at clock to low
-            flash_clk <= 0;
-            flash_mosi <= flash_data_to_send[23];
-            flash_data_to_send <= {flash_data_to_send[22:0], 1'b0};
-            flash_bits_to_send <= flash_bits_to_send - 1;
-            flash_counter <= 1;
-          end else begin
-            // at clock to high
-            flash_counter <= 0;
-            flash_clk <= 1;
-            if (flash_bits_to_send == 0) begin
-              state <= return_state;
-            end
-          end
-        end
-
-        STATE_READ_DATA: begin
-          if (!flash_counter[0]) begin
-            flash_clk <= 0;
-            flash_counter <= flash_counter + 1;
-            if (flash_counter[3:0] == 0 && flash_counter > 0) begin
-              // every 16 clock ticks (8 bit * 2)
-              flash_data_in[flash_current_byte_num] <= flash_current_byte_out;
-              flash_current_byte_num <= flash_current_byte_num + 1;
-              if (flash_current_byte_num == 3) begin
-                state <= STATE_START_WRITE;
-              end
-            end
-          end else begin
-            flash_clk <= 1;
-            flash_current_byte_out <= {flash_current_byte_out[6:0], flash_miso};
-            flash_counter <= flash_counter + 1;
-          end
-        end
-
-        STATE_START_WRITE: begin
-          if (!ramio_busy) begin
-            ramio_enable <= 1;
-            ramio_read_type <= 0;
-            ramio_write_type <= 2'b11;
-            ramio_address <= ramio_address_next;
-            ramio_address_next <= ramio_address_next + 4;
-            ramio_data_in <= {
-              flash_data_in[3], flash_data_in[2], flash_data_in[1], flash_data_in[0]
-            };
-            state <= STATE_WRITE;
-          end
-        end
-
-        STATE_WRITE: begin
-          if (!ramio_busy) begin
-            ramio_enable <= 0;
-            flash_current_byte_num <= 0;
-            if (ramio_address_next < FLASH_TRANSFER_BYTES_NUM) begin
-              state <= STATE_READ_DATA;
-            end else begin
-              flash_cs <= 1;
-              state <= STATE_TEST_1;
-            end
-          end
-        end
-
-        STATE_TEST_1: begin
-          if (!ramio_busy) begin
-            // read unsigned half-word (16 bits) from address 0x4
-            ramio_enable <= 1;
-            ramio_read_type <= 3'b010;
-            ramio_write_type <= 0;
-            ramio_address <= 4;
-            state <= STATE_TEST_2;
-          end
-        end
-
-        STATE_TEST_2: begin
-          if (ramio_data_out_ready) begin
-            if (ramio_data_out == 32'h00_00_41_20) begin  // addr: 0x4, half-word
-              led[4:0] <= 5'b0_0000;
-            end else begin
-              led[4:0] <= 5'b1_1110;
-            end
-            state <= STATE_DONE;
-          end
-        end
-
-        STATE_DONE: begin
-        end
-
-      endcase
-    end
-  end
+      .flash_clk (flash_clk),
+      .flash_miso(flash_miso),
+      .flash_mosi(flash_mosi),
+      .flash_cs  (flash_cs)
+  );
 
 endmodule
 
